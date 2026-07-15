71b04fe97f93dd3af884591d085134ddee4d9ac9
kamal almorsy <mrsykmal@440gmail.com>
2026-07-15 23:14:15 +0300

    16een

--- Inheritance.cpp
+++ Inheritance.cpp
@@ -4,26 +4,87 @@
  #include <algorithm>
  using namespace std;
 
-   class person 
+   class Person 
    {
-     private :
-     string name ;
-     string gender;
-     float age ;
+    
 
      public :
-        person(){}
-        person(string n , string g , float a){}
-       
+       string name ="Unknown" ;
+     string gender ="Unknown";
+        float age = 0 ;
+        Person(){}
+        Person(string n , string g , float a):name(n),gender(g),age(a){}
+         void setname(string  n)
+         {  
+              name = n; 
+         }
+         string getname()
+         {
+             return name;
+         }
+         void setGender(string g)
+         {
+              gender = g;
+         }
+         string getGender()
+         {
+             return gender ;
+         }
+         void setAge(int a)
+         {
+             age = a;
+         }
+         void printDetails()
+         {
+            cout << "name : " << name << " Gender : " << gender << " Age : " << age <<"\n";
+         }
    };
-class Student:public person  // class studnt is a person ,  and ihert all features from base class
+class Student:public Person  // class studnt is a person ,  and ihert all features from base class
    {
-       
+        private :
+         int level;
+          float Gpa ;
+          string department;
+         public :
+          Student(){}
+          Student(int l , float g , string d):level(l),Gpa(g),department(d){}
+         void setLevel(int l)
+         {
+           level = l;
+         }
+         int getLevel( )
+         {
+          return level ;
+         }
+     void setGPA(float g)
+      {
+       Gpa = g ;
+      }
+     float getGPA()
+      {
+      return Gpa ;
+      }
+     void setDepartment(string d)
+      {
+       department = d ;
+      }
+     string getDepartment()
+      {
+      return department ;
+      }
    };
  int main ()
  {
+    // Person p1("ahmed","male" ,22.1); 
     Student s1;
-    
+    s1.printDetails();
+    s1.setAge(22);
+    s1.setname("OMAR");
+    s1.setGender("MALE");
+    s1.printDetails();      
+    s1.setLevel(121);
+     cout << s1.getLevel() << " ... \n";
+     
      
      return 0;
  }
